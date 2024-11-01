7ba0a85446c4ff440ede34da746f04dee3063bda
Md Sajjad Hosen <md.sajjad01733@gmail.com>
2024-11-01 23:40:00 +0600

    Add files via upload

--- Week_1/Day_6/B_Radio_Station.cpp
+++ Week_1/Day_6/B_Radio_Station.cpp
@@ -12,30 +12,22 @@ int main()
     cin.tie(NULL);
     int a, b;
     cin >> a >> b;
-    pair<string, string> mp1[a];
+    map<string, string> mp1;
     for (int i = 0; i < a; i++)
     {
-        cin >> mp1[i].first >> mp1[i].second;
-    }
-    pair<string, string> mp2[b];
-    for (int i = 0; i < b; i++)
-    {
-        cin >> mp2[i].first >> mp2[i].second;
+        string name, ip;
+        cin >> name >> ip;
+        mp1[ip] = name;
     }
     for (int i = 0; i < b; i++)
     {
-        string s1 = mp2[i].second;
-        for (int j = 0; j < a; j++)
-        {
-            string s2 = mp1[j].second;
-            if(s1[s1.size()-2] == s2[s2.size()-1] && s1[s1.size()-3] == s2[s2.size()-2])
-                cout << mp2[i].first << " " << mp2[i].second << " #" << mp1[j].first << endll;
-        }
+        string name, ip;
+        cin >> name >> ip;
+        // mp1[ip] = name;
+        ip.pop_back();
+        cout << name << " " << ip << "; #" << mp1[ip] << endll;
+    }
     
-    }
 
-
-    // string z = mp1[1].first;
-    // cout << z[] << endll;
     return 0;
 }
