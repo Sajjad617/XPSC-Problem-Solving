a695dd63a3176a391399f83b29921430e1c618d4
Md Sajjad Hossain <md.sajjad01733@gmail.com>
2025-02-28 22:47:53 +0600

    Add files via upload

---
+++ Week_16/Day_6/A_FizzBuzz_Remixed.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endll '\n'
+#define Y cout << "YES" << endll
+#define no cout << "NO" << endll
+#define nl cout << endll
+void game()
+{
+    int n;
+    cin >> n;
+    int cal = (n/15)*3;
+    for (int i = 0; i < (n%15)+1; i++)
+    {
+        cal += (i%3==i%5);
+    }
+    cout << cal << endl;
+    
+
+
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        game();
+    }
+    return 0;
+}
